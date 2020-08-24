3d7ebe839593b91b359c665a4d63da4b75794387
mGrapf <mathias.denz@gmail.com>
2020-08-24 22:54:59 +0200

    cleanup

--- examples/io_pass/helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-	char input[20];
-	printf("Please provide your input: ");
-	fgets(input, 20, stdin);
-	printf("Your input was: %s", input);
-	return 0;
-}
