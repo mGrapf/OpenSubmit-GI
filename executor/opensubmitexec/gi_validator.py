002bc497085be57d5910a56c0399c82889963a76
frodo <mathias.denz@gmail.com>
2020-11-22 23:51:10 +0100

    .

--- executor/opensubmitexec/gi_validator.py
+++ executor/opensubmitexec/gi_validator.py
@@ -225,19 +225,18 @@ def validate(job):
 		if config['test_case_n']: # finally read test-case-n in config
 			for n in range(0,config['n_test_cases']):
 				insertRandom(config['test_case_n'],testcases)
+		default = False
 		if testcases == []: # if no tests defined -> test with random numbers
-			defaultinput = True
+			default = True
 			for n in range(0,3):
 				insertRandom("$100*RANDOM",testcases)
-		else:
-			defaultinput = False
-		return testcases
-		
+		return testcases, default
+	
 	
 		
 		
 	# Fuehre das Programm mehrmals mit entsprechenden cases aus
-	testcases = createTests(config)	
+	testcases, defaulttest = createTests(config)	
 	
 	for test in testcases:
 	
@@ -344,7 +343,7 @@ def validate(job):
 					if output_example != output_submission:
 						raise
 			except:
-				if defaultinput == True:
+				if defaulttest == True:
 					job.send_fail_result("Leider erzeugt dein Code eine andere Ausgabe :/\n\n### Erwartete Ausgabe: ###\n{0}\n\n\n### Deine Ausgabe: ###\n{1}".format(original_example, original_submission), "wrong output")
 				else:
 					job.send_fail_result("Leider erzeugt dein Code eine andere Ausgabe :/\n\n###Eingabe: "+test+"\n\n### Erwartete Ausgabe: ###\n{0}\n\n\n### Deine Ausgabe: ###\n{1}".format(original_example, original_submission), "wrong output")
