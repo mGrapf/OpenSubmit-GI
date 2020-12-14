30bb50ea11a8f53e3261c4008b352390a14a68b9
frodo <mathias.denz@gmail.com>
2020-12-14 14:31:56 +0100

    .

--- executor/opensubmitexec/gi_validator.py
+++ executor/opensubmitexec/gi_validator.py
@@ -290,28 +290,16 @@ def validate(job):
 			exit_code_submission, output_submission = job.run_program('./submission')
 		else:
 			# Programme ausführen
-			print(1)
 			running_example = job.spawn_program('./example', [test], echo=config['echo_input'])
-			print(2)
 			running_submission = job.spawn_program('./submission', [test], echo=config['echo_input'])
-			print(3)
 			# Input senden
 			for word in test.split():
 				time.sleep(config['input_time'])
 				running_example.sendline(word)
-				time.sleep(1)
 				running_submission.sendline(word)
-				print(4)
 			# Ausgabe der Programme lesen
 			exit_code_example, output_example = running_example.expect_end()
-			print(5)
-			time.sleep(1)
 			exit_code_submission, output_submission = running_submission.expect_end()
-			
-			print(20)
-			print(exit_code_submission)
-			print(output_submission)
-			
 			l = len(test)
 			output_example = output_example[l+1:]
 			output_submission = output_submission[l+1:]
